f657aff0f03e51c8e6b0e844fd91e38e8439aa09
EdinK1 <edinkaymakchi@gmail.com>
2019-08-24 18:46:00 -0700

    change packge name

--- themes/refilter/index.php
+++ themes/refilter/index.php
@@ -2,7 +2,7 @@
 /**
  * The main template file.
  *
- * @package RED_Starter_Theme
+ * @package Refilter
  */
 
 get_header(); ?>
--- themes/refilter/page.php
+++ themes/refilter/page.php
@@ -2,7 +2,7 @@
 /**
  * The template for displaying all pages.
  *
- * @package RED_Starter_Theme
+ * @package Refilter
  */
 
 get_header(); ?>
