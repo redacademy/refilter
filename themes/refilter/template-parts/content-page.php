184ada4c18851879854b4974ee72129a92835ba8
EdinK1 <edinkaymakchi@gmail.com>
2019-09-04 01:02:54 -0700

    add background image to body and get rid of the sidebar on all pages

--- themes/refilter/template-parts/content-page.php
+++ themes/refilter/template-parts/content-page.php
@@ -8,10 +8,6 @@
 ?>
 
 <main id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
 	<article class="entry-content">
 		<?php the_content(); ?>
 		<?php
