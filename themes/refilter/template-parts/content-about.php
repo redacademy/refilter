efe155981a385a8dec1a6e970a242470f85a2984
rlam1217 <rlam1217@gmail.com>
2019-09-07 16:16:01 -0700

    intial mobile about page styling

--- themes/refilter/template-parts/content-about.php
+++ themes/refilter/template-parts/content-about.php
@@ -9,13 +9,17 @@
 
 <main id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<article class="entry-content">
-	<?php the_post_thumbnail('', ['class' => 'wavy-banner']) ?>
+	
     <section class="wave"></section>
         <div class="about-title">
-            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+            <?php the_title( '<h1 class="about-entry-title">', '</h1>' ); ?>
         </div>
-        
         <?php the_content(); ?>
+
+        <section class="banner-text-container">
+		<h3 class="who_we_are_page_question"><?php the_field('who_we_are_page_question'); ?></h3>			
+		<p class="who_we_are_page_quote"><?php the_field('who_we_are_page_quote'); ?></p>	
+		</section>
 
 		<?php
 			wp_link_pages( array(
