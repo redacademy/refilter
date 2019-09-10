eee341dca4aad7ed6d944cc722500a4b46f6692c
roberto <roberto_esquivel04@outlook.com>
2019-09-10 05:27:44 -0700

    updated styling for projects page

--- themes/refilter/template-parts/content-projects-page.php
+++ themes/refilter/template-parts/content-projects-page.php
@@ -10,63 +10,65 @@
 <main id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<section class="entry-content">
 
-		<section class="our-purpose">
-		<h3 class="purpose-title"><?php the_field('purpose_title'); ?></h3>			
-		<p class="purpose-description"><?php the_field('purpose_description'); ?></p>	
-		</section>
+    <section class="landing container-fluid">
+			<section class="landing-wave"></section>				
+				<h1 class="landing-title"><?php the_field('projects_title'); ?></h1>
+				<h2 class="landing-question"><?php the_field('projects_question'); ?></h2>
+				<h3 class="landing-quote"><?php the_field('projects_quote'); ?></h3>
+				<a class="btn green-full" href="#">Learn More</a>
+            </section>
+            
+            <section class="container-content">
+            <h1 class="projects-title"><?php the_field('project_description_title'); ?></h1>
+            <p class="proejcts-descripton"><?php the_field('projects_description'); ?></p>
+			</section>
+			
 
-		<section class="what-we-do">
-		<h3 class="what-we-do-title"><?php the_field('what_we_do_title'); ?></h3>			
-		<p class="what-we-do-description"><?php the_field('what_we_do_description'); ?></p>	
-		<img src="<?php the_field('what_we_do_image'); ?>;" />
-		</section>
-
-		<section class="wavy-testimonial">
-		<p class="wavy-testimonial-description"><?php the_field('wavy_testimonial_description'); ?></p>
-		<span class="wavy-testimonial-author"><?php the_field('wavy_testimonial_author'); ?></span>	
-		<span class="wavy-testimonial-member"><?php the_field('wavy_testimonial_member'); ?></span>	
-		<p class="wavy-testimonial-event"><?php the_field('wavy_testimonial_event'); ?></p>	
-		</section>
 		
-		<section class="contribute">
-			<h1 class="contribute-title"><?php the_field('contribute_title'); ?></h1>
-			<p class="contribute-description"><?php the_field('contribute_description'); ?></p>
-		</section>
+			<div class="slide-container">
+            <button class="tab-one" onclick="openTab('One')">London</button>
+            <button class="tab-two" style ="background-color:red"onclick="openTab('Two')">assdasd</button>
+            </div>
+			<div id="One" class="tab-one-container" style="display:none">
+			<div class="balls">
+			<div></div>
+			<div></div>
+			<div></div>
+			</div>
+			
 
-			<ul class="contribute-types">
-				<li class="contribute-recycling">
-					<h3 class="contribute-recycling-title"><?php the_field('contribute_recycling_title'); ?></h3>
-					<p class="contribute-recycling-description"><?php the_field('contribute_recycling_description'); ?></p>
-				</li>
-				<li class="contribute-events">
-					<h3 class="contribute-events-title"><?php the_field('contribute_events_title'); ?></h3>
-					<p class="contribute-events-description"><?php the_field('contribute_events_description'); ?></p>
-				</li>
-				<li class="contribute-donation">
-					<h3 class="contribute-donation-title"><?php the_field('contribute_donation_title'); ?></h3>
-					<p class="contribute-donation-description"><?php the_field('contribute_donation_description'); ?></p>
-				</li>
-				<li class="contribute-word">
-					<h3 class="contribute-word-title"><?php the_field('contribute_word_title'); ?></h3>
-					<p class="contribute-word-description"><?php the_field('contribute_word_description'); ?></p>
-				</li>	
-			</ul>
+            </div>
 
-			<section class="butt-count">
-			<h1 class="butt-count-title"><?php the_field('butt_count_title'); ?></h1>
-			<span class="butt-count-butts-collected"><?php the_field('butts_collected'); ?></span>						
-			<span class="butt-count-waste-collected"><?php the_field('waste_collected'); ?></span>						
+			<div id="Two" class="tab-two-container" style="display:none">
+			<div class="dots">
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			</div>
+ 
+            </div>
+
+
+				
+			</div>
+
+            <section class ="container-content"> 
+			<h1 class="project-partner-title"><?php the_field('project_partner_title'); ?> </h1>
+			<div class="partners-container">
+				
+			<h2 class="partner-one"> <?php the_field('partner_one'); ?>  </h2>   
+			<h2 class="partner-two"> <?php the_field('partner_two'); ?>  </h2>   
+			<h2 class="partner-three"> <?php the_field('partner_three'); ?>  </h2>   
+			</div>
 			</section>
+			
 
-			<ul class="instagram-feed">
-				<li class="instagram_feed_image"><img src="<?php the_field("instagram_feed_image"); ?>" /></li>
-			</ul>
-
-			<section class="wavy-newsletter">
-				<h1 class="wavy-newsletter-title"><?php the_field('newsletter_title'); ?></h1>										
-				<input class="wavy-newsletter-name" type="text" placeholder="<?php the_field('newsletter_name'); ?>" />
-				<input class="wavy-newsletter-email" type="text" placeholder="<?php the_field('newsletter_email'); ?>" />													
-			</section>
 	</section><!-- .entry-content -->
 </main><!-- #post-## -->
 
