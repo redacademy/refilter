4376993498d835ce36843099e329a66a1cf8fd8d
EdinK1 <edinkaymakchi@gmail.com>
2019-09-12 00:23:21 -0700

    structure get involved page

---
+++ themes/refilter/template-parts/content-get-involved-page.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @package Refilter
+ */
+
+?>
+
+<main id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<section class="entry-content">
+	
+	</section><!-- .entry-content -->
+</main><!-- #post-## -->
+
