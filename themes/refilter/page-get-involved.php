98235fc7e2c7fced2054defdb4db2af2f39f4c51
EdinK1 <edinkaymakchi@gmail.com>
2019-09-12 15:46:00 -0700

    add fields for the get involved page

--- themes/refilter/page-get-involved.php
+++ themes/refilter/page-get-involved.php
@@ -9,15 +9,9 @@ get_header(); ?>
 
 	<main id="primary" class="content-area">
 		<section id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'get-involved-page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
-
+			
+			<?php get_template_part( 'template-parts/content', 'get-involved-page' ); ?>
 		</section><!-- #main -->
 	</main><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
