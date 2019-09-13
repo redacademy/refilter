b06935c77779297b4c33c727461c1fdd86cba963
rlam1217 <rlam1217@gmail.com>
2019-09-12 20:28:24 -0700

    creates blog page

--- themes/refilter/home.php
+++ themes/refilter/home.php
@@ -12,12 +12,14 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			
+
+				<?php get_template_part( 'template-parts/content', 'blog' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
 
 		</section><!-- #main -->
 	</main><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
--- themes/refilter/template-parts/content-single.php
+++ themes/refilter/template-parts/content-single.php
@@ -18,7 +18,35 @@
 	</header><!-- .entry-header -->
 
 	<section class="entry-content">
+		
+
+		<?php 
+
+$fields = get_fields();
+$image = get_field('image'); // assigns the image field to the variable of $image
+
+
+if( $fields ): ?>
+    <ul>
+		<?php foreach( $fields as $name => $value
+		
+		): ?>
 		<?php the_content(); ?>
+            <li><?php echo $value; ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+
+
+
+<?php 
+$image = get_field('image'); // assigns the image field to the variable of $image
+if( !empty($image) ){ ?> <!-- if the $image variable isn't empty, display the following: -->
+
+    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" /> <!--displays the URL for the image variable and also the alt tag which is entered in the WordPress media library-->
+
+<?php }; ?> <!--ends the if statement -->
+
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
