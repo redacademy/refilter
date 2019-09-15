e2bc70fd8981cc8859e06a5f161b9cb87bdea0b3
roberto <roberto_esquivel04@outlook.com>
2019-09-15 16:07:42 -0700

    updated refilter plugin

---
+++ themes/refilter/single-projects.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package Refilter
+ */
+
+get_header(); ?>
+
+	<main id="primary" class="content-area">
+		<section id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+
+			<?php the_post_navigation(); ?>
+
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			?>
+
+		<?php endwhile; // End of the loop. ?>
+
+		</section><!-- #main -->
+	</main><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
