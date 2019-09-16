a8552ab07e7d17bc18b23103f8ad54dec5e6855d
rlam1217 <rlam1217@gmail.com>
2019-09-15 23:32:43 -0700

    adds responsive design to posts page

--- themes/refilter/template-parts/content-single.php
+++ themes/refilter/template-parts/content-single.php
@@ -4,40 +4,59 @@
  *
  * @package Refilter
  */
-
 ?>
 
 <main id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 	</header><!-- .entry-header -->
-	<style> 
+	
+<style> 
+@media only screen and (min-width: 768px) {
 		.background-container {
 		background: linear-gradient(180deg,rgba(0,0,0,.25) 0,rgba(0,0,0,.25));	
 		background: url('<?php the_field('landing_blog_post_image_desktop'); ?>') no-repeat center center/cover;
-		
-			}
-	</style> 
+		}
+		.mobile { display: none;
+		} 
+		.blog-entry-content  { display: none;} 
+		}
 
-	<div class="background-container">
-		<?php the_field('landing_blog_post_image_desktop'); ?>
+		@media only screen and (max-width: 767px) {
+		.landing {
+		background: url('<?php the_field('landing_blog_post_image_mobile'); ?>') no-repeat center center/cover;
+		}	
+		.background-container { display: none;}
+		}
+</style> 	
 
-		<section class="posts landing container-fluid">
+
+<div class="background-container">
+	<section class="desktop landing container-fluid">
+		<h2><?php the_field('blog_post_title'); ?></h2>
+		<p><?php the_field('blog_post_description'); ?></p>
+	</section>			
+	
+	<section class="blog-entry-content-desktop-wrapper"> 
+				<p><?php the_field('blog_post_content'); ?></p>
+				<img src="<?php the_field('blog_post_content_left_image'); ?>" />
+			<img src="<?php the_field('blog_post_content_right_image'); ?>" />
+	</section>
+</div>		
+
+<section class="mobile landing container-fluid">
 			<section class="landing-wave"></section>
 				<h2><?php the_field('blog_post_title'); ?></h2>
 				<p><?php the_field('blog_post_description'); ?></p>
-		</section>
-		
-		
-		<section class="blog-entry-content">
+</section>		
+
+<section class="blog-entry-content">
+	<p><?php the_field('blog_post_content'); ?></p>
 		<img src="<?php the_field('blog_post_content_left_image'); ?>" />
-				<img src="<?php the_field('blog_post_content_right_image'); ?>" />
-
-		</section>
-
-	</div>
-	
-	
-	<footer class="entry-footer">
-		<?php refilter_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+		<img src="<?php the_field('blog_post_content_right_image'); ?>" />
+</section>
+		
+		
+<footer class="entry-footer">
+	<?php refilter_entry_footer(); ?>
+</footer><!-- .entry-footer -->
 </main><!-- #post-## -->
