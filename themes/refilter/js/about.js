79f445a8242fe6de6abfae30d4ac436de1f8d49b
rlam1217 <rlam1217@gmail.com>
2019-09-19 17:02:09 -0700

    adds acf repeater to about page

--- themes/refilter/js/about.js
+++ themes/refilter/js/about.js
@@ -6,72 +6,16 @@
             $(".up-arrow").show().click(function(){
                     $(".down-arrow-1").show()
               $(".up-arrow").fadeOut()
-              $(".staff-member-1-mobile-description").fadeOut()
+              $(".staff-member-mobile-description").fadeOut()
               $(".mobile-fun-fact-header").fadeOut()
-              $(".staff-member-1-mobile-fun-fact").fadeOut()
+              $(".staff-member-mobile-fun-fact").fadeOut()
             });
           })
-  $(".staff-member-1-mobile-description").show();
+  $(".staff-member-mobile-description").show();
   $(".mobile-fun-fact-header").show();
-  $(".staff-member-1-mobile-fun-fact").show();
+  $(".staff-member-mobile-fun-fact").show();
   $(".down-arrow-1").show()
     });
-
-
-
-    $(".down-arrow-2").click(function() {
-        $(".down-arrow-2").fadeOut(function(){
-            $(".up-arrow-2").show().click(function(){
-                    $(".down-arrow-2").show()
-              $(".up-arrow-2").fadeOut()
-              $(".staff-member-2-mobile-description").fadeOut()
-              $(".mobile-fun-fact-header").fadeOut()
-              $(".staff-member-2-mobile-fun-fact").fadeOut()
-            });
-          })
-  $(".staff-member-2-mobile-description").show();
-  $(".mobile-fun-fact-header").show();
-  $(".staff-member-2-mobile-fun-fact").show();
-  $(".down-arrow-2").show()
-    });
-
-
-
-    $(".down-arrow-3").click(function() {
-        $(".down-arrow-3").fadeOut(function(){
-            $(".up-arrow-3").show().click(function(){
-                    $(".down-arrow-3").show()
-              $(".up-arrow-3").fadeOut()
-              $(".staff-member-3-mobile-description").fadeOut()
-              $(".mobile-fun-fact-header").fadeOut()
-              $(".staff-member-3-mobile-fun-fact").fadeOut()
-            });
-          })
-  $(".staff-member-3-mobile-description").show();
-  $(".mobile-fun-fact-header").show();
-  $(".staff-member-3-mobile-fun-fact").show();
-  $(".down-arrow-3").show()
-    });
-
-
-    
-    $(".down-arrow-4").click(function() {
-        $(".down-arrow-4").fadeOut(function(){
-            $(".up-arrow-4").show().click(function(){
-                    $(".down-arrow-4").show()
-              $(".up-arrow-4").fadeOut()
-              $(".staff-member-4-mobile-description").fadeOut()
-              $(".mobile-fun-fact-header").fadeOut()
-              $(".staff-member-4-mobile-fun-fact").fadeOut()
-            });
-          })
-  $(".staff-member-4-mobile-description").show();
-  $(".mobile-fun-fact-header").show();
-  $(".staff-member-4-mobile-fun-fact").show();
-  $(".down-arrow-4").show()
-    });
-
-
 
 
 
