2c6c2a71849a44a17b73e3b025514f3f68794195
rlam1217 <rlam1217@gmail.com>
2019-09-21 13:38:29 -0700

    general styling on the blog page

--- themes/refilter/home.php
+++ themes/refilter/home.php
@@ -15,13 +15,23 @@ get_header(); ?>
         @media only screen and (min-width: 768px) {
             .landing {
 		background: url('<?php the_field('blog_landing_image_desktop', get_option('page_for_posts')); ?>') no-repeat center center/cover;
-		}	
+		background-size: 100% 100%;
+		}
+		
+		.wavy-newsletter {
+			background: url('<?php the_field('newsletter_image_desktop', get_option('page_for_posts')); ?>') no-repeat center center/cover;
+		}		
 		
 		}
 
 		@media only screen and (max-width: 767px) {
 		.landing {
 		background: url('<?php the_field('blog_landing_image_mobile', get_option('page_for_posts')); ?>') no-repeat center center/cover;
+		background-size: 100% 100%;
+		}
+		
+		.wavy-newsletter {
+			background: url('<?php the_field('newsletter_image_mobile', get_option('page_for_posts')); ?>') no-repeat center center/cover;
 		}	
 		
 		}
@@ -30,8 +40,8 @@ get_header(); ?>
 		<section id="main" class="site-main" role="main">
 
 		<section class="blog landing container-fluid">
-			<h1 class="landing-title"><?php the_field('blog_page_title1', get_option('page_for_posts')); ?></h1>		
-			<h3 class="landing-quote"><?php the_field('blog_page_description1', get_option('page_for_posts')); ?></h3>
+			<h1 class="landing-title"><?php the_field('blog_page_title', get_option('page_for_posts')); ?></h1>		
+			<h3 class="landing-quote"><?php the_field('blog_page_description', get_option('page_for_posts')); ?></h3>
 		</section>
 
 		<div class="content-space"></div>
@@ -44,10 +54,10 @@ get_header(); ?>
 		</section><!-- #main -->
 
 		<section class="wavy-newsletter container-fluid">
-				<h1 class="wavy-newsletter-title"><?php the_field('newsletter_title'); ?></h1>										
+				<h1 class="wavy-newsletter-title"><?php the_field('newsletter_title', get_option('page_for_posts')); ?></h1>										
 				<form class="wavy-newsletter-form">
-				<input class="wavy-newsletter-name" type="text" placeholder="<?php the_field('newsletter_name'); ?>" />
-				<input class="wavy-newsletter-email" type="text" placeholder="<?php the_field('newsletter_email'); ?>" />													
+				<input class="wavy-newsletter-name" type="text" placeholder="<?php the_field('newsletter_name', get_option('page_for_posts')); ?>" />
+				<input class="wavy-newsletter-email" type="text" placeholder="<?php the_field('newsletter_email', get_option('page_for_posts')); ?>" />													
 				<button class="btn green-empty" type="submit">Join Mailing List</button>
 			</form>
 		</section>
