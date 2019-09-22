7d3094104d6b6b9f7b3cbf37a0566dbbf4610ce9
Alex Hing Liu <hingliu6@gmail.com>
2019-09-22 00:42:45 -0700

    initial structure for single events start

--- themes/refilter/template-parts/content-single-events.php
+++ themes/refilter/template-parts/content-single-events.php
@@ -7,50 +7,60 @@
 ?>
 
 <main id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-	</header><!-- .entry-header -->
 	
-<style> 
-@media only screen and (min-width: 768px) {
-		.full-background {
-		background: linear-gradient(180deg,rgba(0,0,0,.25) 0,rgba(0,0,0,.25));	
-		background: url('<?php the_field('landing_blog_post_image_desktop'); ?>') no-repeat center center/cover;
-		}
-		
-		
+	<style> 
+		@media only screen and (min-width: 768px) {
+			.full-background {
+			background: linear-gradient(180deg,rgba(0,0,0,.25) 0,rgba(0,0,0,.25));	
+			background: url('<?php the_field('single_event_background_image_desktop'); ?>') no-repeat center center/cover;
+			}
 		}
 
 		@media only screen and (max-width: 767px) {
-		.landing {
-		background: url('<?php the_field('landing_blog_post_image_mobile'); ?>') no-repeat center center/cover;
-		}	
-		
+			.landing {
+			background: url('<?php the_field('single_event_background_image_mobile'); ?>') no-repeat center center/cover;
+			}		
 		}
-</style> 	
+	</style> 	
 
-<p>TESTING</p>
-
-<section class="mobile landing container-fluid">
-			<section class="landing-wave"></section>
-				<h2><?php the_field('blog_post_title'); ?></h2>
-				<p><?php the_field('blog_post_description'); ?></p>
-</section>		
-
-<section class="blog-entry-content">
-	<p><?php the_field('blog_post_content'); ?></p>
-	<div class="overlapping-content">
-		<div class="left-image">	
-			<img src="<?php the_field('blog_post_content_left_image'); ?>" />
-		</div>	
-		<div class="right-image">
-			<img src="<?php the_field('blog_post_content_right_image'); ?>" />
-		</div>	 
+	<section class="landing-single-event container">
+		<h1 class="landing-single-event-title"><?php the_field('event_title'); ?></h1>	
+		<h3 class="landing-single-event-components"><?php the_field('event_landing_date'); ?></h3>
+		<h3 class="landing-single-event-components"><?php the_field('event_landing_time'); ?></h3>
+		<h3 class="landing-single-event-components"><?php the_field('event_landing_location'); ?></h3>
+		<h3 class="landing-single-event-components"><?php the_field('event_landing_joined_count'); ?></h3>				
+	</section>
+	
+	<div class="rsvp">
+		<a class="rsvp-btn" href="#">RSVP Now<i class="fas fa-arrow-right"></i></a>
 	</div>
 
-	<h2>Related Posts</h2>
-	<div class="related-posts-wrapper">
+	<section class="mobile landing container-fluid">
+		
+		<section class="landing-wave"></section>
+			<h2><?php the_field(''); ?></h2>
+			<p><?php the_field(''); ?></p>
+		</section>		
+
+		<section class="blog-entry-content">
+			<p><?php the_field('blog_post_content'); ?></p>
 			
-<?php
+			<div class="overlapping-content">
+				
+				<div class="left-image">	
+					<img src="<?php the_field('blog_post_content_left_image'); ?>" />
+				</div>	
+				
+				<div class="right-image">
+					<img src="<?php the_field('blog_post_content_right_image'); ?>" />
+				</div>
+
+			</div>
+
+			<h2>Related Posts</h2>
+			<div class="related-posts-wrapper">	
+	<?php
+
 // Default arguments
 $args = array(
 	'posts_per_page' => 2, // How many items to display
@@ -72,7 +82,7 @@ if ( ! empty( $cats_ids ) ) {
 $wpex_query = new wp_query( $args );
 
 // Loop through posts
-foreach( $wpex_query->posts as $post ) : setup_postdata( $post ); ?>
+foreach( $wpex_query->posts as $events ) : setup_postdata( $events ); ?>
 	
 		
 			<div class="related-posts-image-wrapper">
